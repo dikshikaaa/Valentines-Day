44e8dab4412d7596c5ba10b3e8ba0bc1e8d8a214
Dikshika Dweepanita <91119764+dikshikaaa@users.noreply.github.com>
2024-02-04 19:06:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,35 +34,45 @@ export default function Page() {
   };
 
   return (
-    <div className="valentine-container">
-      {yesPressed ? (
-        <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="text-container">Ok yay!!!</div>
-        </>
-      ) : (
-        <>
-          <img
-            className="h-[200px]"
-            style={{ width: "400x", height: "240px" }}
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-          />
-          <h1 className="text-container">Will you be my Valentine?</h1>
-          <div>
-            <button
-              className={"yes-button"}
-              style={{ fontSize: yesButtonSize }}
-              onClick={() => setYesPressed(true)}
-            >
-              Yes
-            </button>
+    <div className="centered-container">
+      <a
+        href="https://github.com/dikshikaaa/Valentine-s-day-experiment"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-link"
+      >
+        Visit Source Code on GitHub
+      </a>
+      <div className="valentine-container">
+        {yesPressed ? (
+          <>
+            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+            <div className="text-container">Ok yay!!!</div>
+          </>
+        ) : (
+          <>
+            <img
+              className="h-[200px]"
+              style={{ width: "400x", height: "240px" }}
+              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            />
+            <h1 className="text-container">Will you be my Valentine?</h1>
+            <div>
+              <button
+                className={"yes-button"}
+                style={{ fontSize: yesButtonSize }}
+                onClick={() => setYesPressed(true)}
+              >
+                Yes
+              </button>
 
-            <button onClick={handleNoClick} className="no-button">
-              {noCount === 0 ? "No" : getNoButtonText()}
-            </button>
-          </div>
-        </>
-      )}
+              <button onClick={handleNoClick} className="no-button">
+                {noCount === 0 ? "No" : getNoButtonText()}
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
